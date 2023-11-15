98c1a58acd75d0b26eb1bb66b47001521154971e
Amiroo <amir.charkhand@outlook.com>
2023-11-15 16:02:39 +0330

    BrowserCheck Middle ware comented so Microsoft edge can also connect to backend. .sln file added for visual studio

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseBrowserCheck();
+//app.UseBrowserCheck();
 
 if (app.Environment.IsDevelopment())
 {
